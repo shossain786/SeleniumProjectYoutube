f5e2cf1ea9c7e19cd8d42577c3de54143cdd7f06
Mohammad Saddam Hossain <sddmhossain786@gmail.com>
2024-05-26 17:16:11 +0530

    locator code added

--- src/test/java/org/saddy/LocatorDemo.java
+++ src/test/java/org/saddy/LocatorDemo.java
@@ -3,25 +3,28 @@ package org.saddy;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
 
 public class LocatorDemo {
-    @Test
-    public void locatorTest() throws InterruptedException {
-        WebDriver driver = new ChromeDriver();
+    WebDriver driver;
+    @BeforeMethod
+    public void setUP() {
+        driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
         driver.get("http://localhost:8080/iTinstruct-1.3.5/home.html");
+    }
+    @Test
+    public void locatorTest() throws InterruptedException {
+        //enter text into a input field: id = inputField
 
-        //enter text into the Job details field
-
-        driver.findElement(By.id("designation")).sendKeys("Test data 123");
+        driver.findElement(By.id("inputField")).sendKeys("1234");
 
 
         Thread.sleep(3000);
-
         driver.quit();
     }
 }
